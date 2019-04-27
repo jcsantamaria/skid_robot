edc30b6e162d0534fd29495fbdd524c33abe52bf
Juan Carlos Santamaria <jcsantamaria@trimble.com>
2019-04-27 23:26:03 +0000

    Add dependency to pca9685 module

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages=['skid_robot'],
     package_dir={'': 'src'},
+    install_requires=[‘adafruit-pca9685’]
 )
 
 setup(**setup_args)
